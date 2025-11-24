2a75dc8b7adfb54ee85192a36c030ad2193c5b28
Thanusuyasri <82963993+Thanusuyasri@users.noreply.github.com>
2025-11-24 13:12:31 +0530

    modified wsgi.py for migrations

--- cloud_storage/wsgi.py
+++ cloud_storage/wsgi.py
@@ -1,16 +1,13 @@
-"""
-WSGI config for cloud_storage project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
-"""
-
 import os
+import django
+from django.core.management import call_command
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'cloud_storage.settings')
+django.setup()
+
+try:
+    call_command("migrate", interactive=False)
+except Exception as e:
+    print(f"Migration error: {e}")
 
 from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'cloud_storage.settings')
-
 application = get_wsgi_application()
